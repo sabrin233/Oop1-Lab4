2eaee0f4dafdd10a90e0a6df9ead84068a03de0d
sabrin233 <61112185+sabrin233@users.noreply.github.com>
2020-02-21 05:42:53 -0800

    commiting a new source code
    add some rules

--- src/oop1Lab4/StudentTest.java
+++ src/oop1Lab4/StudentTest.java
@@ -4,11 +4,6 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		
-		String id;
-		String name;
-		String Department;
-		float Cgpa;
-		
 		Student s1 = new Student();
 		s1.setId("17-34472-2");
 		s1.setName("Rashma");
