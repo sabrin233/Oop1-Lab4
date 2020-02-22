5a56426fbddd82c626c63a32ec640af1b4b97545
sabrin233 <61112185+sabrin233@users.noreply.github.com>
2020-02-22 00:15:32 -0800

    commiting a source code
    add a new line

--- src/oop1Lab4/AccountTest.java
+++ src/oop1Lab4/AccountTest.java
@@ -6,10 +6,10 @@ public class AccountTest {
 		Account a1 = new Account("Bank","17-34",10000);
 		a1.withdraw(1000);
 		a1.deposit(5000);
-		System.out.println(a1);
+		a1.showinfo();
 		
 
 
-	}
+}
 
 }
