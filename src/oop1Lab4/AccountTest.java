186e7bc5bb30e8a0c7e4c1f3552bf475aace7a7f
sabrin233 <61112185+sabrin233@users.noreply.github.com>
2020-02-21 19:52:06 -0800

    commiting a new source code
    add a new code

--- src/oop1Lab4/AccountTest.java
+++ src/oop1Lab4/AccountTest.java
@@ -2,16 +2,10 @@ package oop1Lab4;
 
 public class AccountTest {
 	
-	String accName;;
-	String acId;
-	int balance;
-	int deposite;
-	int withdraw;
-	
 	public static void main(String[] args){
-		Account a1 = new Account();
-		a1.withdraw(250);
-		a1.deposit(400);
+		Account a1 = new Account("Bank","17-34",10000);
+		a1.withdraw(1000);
+		a1.deposit(5000);
 		System.out.println(a1);
 		
 
